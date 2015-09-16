9493f617e4f97cceb5ff0f086ad31f2ea06236fb
pi-georgia <gellow@gellows-MBP.lan>
2015-09-16 16:24:26 +0300

    rfresh

---
+++ Submit Project.R
@@ -0,0 +1,13 @@
+the<-read.csv("pml-testing.csv")
+theme<-predict(gbmfit,the)
+
+
+pml_write_files = function(x){
+  n = length(x)
+  for(i in 1:n){
+    filename = paste0("problem_id_",i,".txt")
+    write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+  }
+}
+
+pml_write_files(theme)
